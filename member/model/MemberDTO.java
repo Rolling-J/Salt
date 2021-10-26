6068ddb1d2066854ba81e2cc7f01d3e0eca5c5b7
Rolling-J <dbwls45888@naver.com>
2021-10-27 02:16:43 +0900

    폴더정리, member DAO DTO 추가

---
+++ member/model/MemberDTO.java
@@ -0,0 +1,19 @@
+package model;
+
+public class MemberDTO{
+
+    private String id;
+	private String password;
+	private String name;
+	private String birth;
+	private String gender;
+	private String email;
+	private String phone;
+	private String ip;
+	
+	public MemberDTO() {
+		super();
+	}
+//getter setter 필요
+
+}
